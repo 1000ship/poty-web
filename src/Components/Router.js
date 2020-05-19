c7f6995f104c0f725fb7af83130e32546e308755
1000ship <cjstjdgur1234@gmail.com>
2020-05-19 19:29:54 +0900

    Change BrowerRouter to HashRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Header from 'Components/Header'
 import Home from 'Routes/Home'
 import Theater from 'Routes/Theater'
