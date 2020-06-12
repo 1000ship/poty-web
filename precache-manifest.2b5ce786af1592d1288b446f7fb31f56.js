946c549c1f7219dd7c4d63124c1e1746804958d1
1000ship <cjstjdgur1234@gmail.com>
2020-06-12 21:09:24 +0900

    Updates

---
+++ precache-manifest.2b5ce786af1592d1288b446f7fb31f56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f5ab0cd2ebfdb3a8615be076a4d668",
+    "url": "/poty-web/index.html"
+  },
+  {
+    "revision": "fb69b1bbf4ee29420d41",
+    "url": "/poty-web/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "7128bf02c04ee5a31cae",
+    "url": "/poty-web/static/css/main.6135df4c.chunk.css"
+  },
+  {
+    "revision": "fb69b1bbf4ee29420d41",
+    "url": "/poty-web/static/js/2.20b0c0ec.chunk.js"
+  },
+  {
+    "revision": "5c613c7669dcbf9b7ec38a684e605a06",
+    "url": "/poty-web/static/js/2.20b0c0ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7128bf02c04ee5a31cae",
+    "url": "/poty-web/static/js/main.44ba69d7.chunk.js"
+  },
+  {
+    "revision": "2014ab9a26d1fb033c30",
+    "url": "/poty-web/static/js/runtime-main.359bc62d.js"
+  }
+]);
